793d1b7f8c9bb2de3417170d81dd5d2af1d6238e
fuglerud <majen78@gmail.com>
2020-04-27 00:06:58 +0200

    upped scala version to 2.12.0

--- src/test/scala/magnus/DemoSimulation.scala
+++ src/test/scala/magnus/DemoSimulation.scala
@@ -8,7 +8,7 @@ import io.gatling.http.Predef._
 class DemoSimulation extends Simulation{
 
   val httpConf = http
-
+    .baseUrl("http://computer-database.gatling.io")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
@@ -17,7 +17,7 @@ class DemoSimulation extends Simulation{
 
   val scn = scenario("Demoimulation")
 
-    .feed(csv("demoSim_Computers.csv").circular)
+    .feed(csv("data/demoSim_Computers.csv").circular)
 
     .exec(http("Base")
     .get("/")
